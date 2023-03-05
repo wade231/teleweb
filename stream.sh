98514169f7e262080e2ca1907c516cf4c4730f7b
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-05 18:31:05 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i MyVideo.mp4 -c:v libx264 -preset veryfast -c:a aac -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
+ffmpeg -re -i “https://crichdplayer.xyz/embed2.php?id=skysp2” -c:v libx264 -preset veryfast -c:a aac -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
 done
