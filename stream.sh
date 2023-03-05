fea38c05b9b60285b1eb533e0c15893982dcb00c
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-05 16:33:45 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,17 @@ set -e
 
 while true
 do
-ffmpeg -re -i MyVideo.mp4 -c:v libx264 -preset veryfast -c:a aac -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
+ffmpeg -stream_loop -1 -re -i https://drive.google.com/uc?export=download&id=1Asy0VKnyrva7uVzXJMw3FTdmMUPQtgav
+-c:v libx264 
+-preset veryfast 
+-b:v 3500k 
+-maxrate 3500k 
+-bufsize 7000k 
+-pix_fmt yuv420p 
+-g 50 
+-c:a aac 
+-b:a 160k 
+-ac 2 
+-ar 44100 
+-f flv rtmps://dcx-y.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
 done
