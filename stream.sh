3fc0c6a53a14ebc18dfeea6c9d16eaa05922dca4
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-06 16:42:35 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i https://crichdplayer.xyz/embed2.php?id=btsp3 -c:v copy -c:a copy -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
+ffmpeg -i https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8 -c copy -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
 done
