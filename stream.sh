078ccf56d0b07f783779f78e822b57c191546c2e
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-06 16:59:39 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8 -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
+ffmpeg -re -i https://cbsnews.akamaized.net/hls/live/2020607/cbsnlineup_8/master.m3u8 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/926739692:_Czm-Fbv5bbF2Npo7YaDLg
 done
