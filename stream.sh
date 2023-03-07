5946645261030d25b39160d2cd79f47a94b81365
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-07 16:04:15 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i "https://es6.quest123.top:8088/live/webcricu19/playlist.m3u8?id=115680&pk=97c500581dcf174d9c131ce5501f933acfbe01b16d11f374cadf78c674c2bdef2a6d0b7791aa733ea5e50dab88918f9ab7cb45d7010e408d46464280a44278a1" -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1709976325:_HeNVWeSSIs2bA_34jacWg
+ffmpeg -re -i https://lin-gd-001-cf.slivcdn.com/out/v1/7394bf66d54d42f09b7cc0ed4bb7e9e9/master_2.m3u8?hdntl=exp=1678227932~acl=*~hmac=ff6a3619489091c567610d5c3d9ef1067d02be3c88027cb69751b993b1c53410&originpath=/linear/hls/pb/event/wG75n5U8RrOKiFzaWObXbA/stream/94217428-3da7-4a3d-b2db-9245a3c5557c:SIN2/variant/62468529bc23c4ccf6bf0c250a29d2bf/bandwidth/1828829.m3u8 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
 done
