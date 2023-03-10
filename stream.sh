204742fab6c02dd01a285f6db6e0af7c410d8dd5
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-10 17:55:00 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,12 @@ set -e
 
 while true
 do
-ffmpeg -re -stream_loop -1 -i 'https://www.youtube.com/watch?v=KQ3Y0wbAtHE' -c:v libx264 -b:v 600k -r 25 -s 640x360 -c:a aac -ab 64k -ac 1 -ar 44100 -vf setpts=N/FRAME_RATE/TB -af asetpts=N/SR/TB -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
+ffmpeg \
+    -re -f lavfi -i "movie=filename=318.mp4:loop=0, setpts=N/(FRAME_RATE*TB)" \
+    -thread_queue_size 512 -i "https://radio.lotustechnologieslk.net:2020/stream/hirufmgarden" \
+    -map 0:v:0 -map 1:a:0 \
+    -map_metadata:g 1:g \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+    -f flv "rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA"
 done
