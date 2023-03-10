4a035685def8a6a52cc9847629532e3d27e42298
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-10 17:24:04 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -stream_loop -1 -i https://www.youtube.com/watch?v=KQ3Y0wbAtHE -c:v libx264 -b:v 600k -r 25 -s 640x360 -c:a aac -ab 64k -ac 1 -ar 44100 -vf setpts=N/FRAME_RATE/TB -af asetpts=N/SR/TB -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
+ffmpeg -re -stream_loop -1 -i 'https://www.youtube.com/watch?v=KQ3Y0wbAtHE' -c:v libx264 -b:v 600k -r 25 -s 640x360 -c:a aac -ab 64k -ac 1 -ar 44100 -vf setpts=N/FRAME_RATE/TB -af asetpts=N/SR/TB -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
 done
