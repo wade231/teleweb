5d6dd7c7419db2a27bf865ff9283e167faac1402
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-10 18:01:32 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -9,7 +9,8 @@ ffmpeg \
     -thread_queue_size 512 -i "https://radio.lotustechnologieslk.net:2020/stream/hirufmgarden" \
     -map 0:v:0 -map 1:a:0 \
     -map_metadata:g 1:g \
-    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
-    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
-    -f flv "rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA"
+    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
 done
