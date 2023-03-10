d8d01441404ab8a3daa956d94b3a18fbde89aa90
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-03-10 18:37:08 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,11 +4,9 @@ set -e
 
 while true
 do
-ffmpeg \
-    -re -f image2 -loop 1 -i 318.png \
+  ffmpeg -loglevel info -y -re \
+    -f image2 -loop 1 -i bg.png \
     -thread_queue_size 512 -i "https://radio.lotustechnologieslk.net:2020/stream/hirufmgarden" \
-    -map 0:v:0 -map 1:a:0 \
-    -map_metadata:g 1:g \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
     -c:a aac -b:a 128k -ar 44100 \
