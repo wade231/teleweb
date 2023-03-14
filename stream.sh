a745f07ecbcc4fa3052b7aa72c387e8258289d67
wade231 <122469077+wade231@users.noreply.github.com>
2023-03-14 13:24:03 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,13 +4,5 @@ set -e
 
 while true
 do
- ffmpeg \
-    -re -f lavfi -i "movie=filename=318.mp4:loop=0, setpts=N/(FRAME_RATE*TB)" \
-    -thread_queue_size 512 -i "https://radio.lotustechnologieslk.net:2020/stream/hirufmgarden" \
-    -map 0:v:0 -map 1:a:0 \
-    -map_metadata:g 1:g \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 25 -video_size 480×360 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
+ffmpeg -re -i 'https://lin-gd-001-cf.slivcdn.com/out/v1/f88af37f57d94cbd8c584cd5e83d92aa/master_6.m3u8?hdntl=exp=1678823306~acl=*~hmac=28e52df263eff103d0d783ed9481e63cc26a05ef4e68ef975efca53051185db5&originpath=/linear/hls/pb/event/x7rXWd2ERZ2tvyQWPmO1HA/stream/633e5cf9-709e-408b-b08c-7bc99435c7b2:SIN/variant/85776a91b3eddd067623d8028b35e375/bandwidth/1245230.m3u8' -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
 done
