6ae00444d139b23df327d5a6ec138689c8e74f5a
wade231 <122469077+wade231@users.noreply.github.com>
2023-10-02 16:42:44 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i 'https://d2j1554jr9lrly.cloudfront.net/live/74c36069-cbbf-4036-b3ea-4fb8334d2692/live.isml/74c36069-cbbf-4036-b3ea-4fb8334d2692.m3u8' -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1939768651:1OBhYuPJBg6d10PNvE1D2A
+ffmpeg -re -i 'https://dai.fancode.com/linear/hls/pb/event/wqN_hWjTTBK-MaOnCOTvog/stream/cac78c13-ca3f-4b4b-9f2d-71436affada0:TPE/variant/cebf3d1e8deb1171147ac640c14605ae/bandwidth/1756808.m3u8' -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1939768651:1OBhYuPJBg6d10PNvE1D2A
 done
