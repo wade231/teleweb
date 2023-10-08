de0db88b9a17dd34eff822944dd64cff38180429
wade231 <122469077+wade231@users.noreply.github.com>
2023-10-08 17:52:22 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ set -e
 
 while true
 do
-ffmpeg -re -i 'https://tvs1.aynaott.com/sspts1/tracks-v1a1/mono.m3u8?hdnts=st=1696758233~exp=1696801433~acl=/*~data=45.61.146.183-WEB~hmac=8bae0676793cdb6b821b46e4a90bb32b048d729bd8b65d42ce440181a47977a0' -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
+ffmpeg -re -i 'https://tvs2.aynaott.com/Ptvsports/tracks-v1a1/mono.m3u8?hdnts=st=1696767698~exp=1696810898~acl=/*~data=45.61.146.183-WEB~hmac=0f69a398da73c71e49dfb6842cf84a6dd27ea47db011f4d236973c6099b02530' -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmps://dc5-1.rtmp.t.me/s/1232369957:_ZNvpDHr6Fi2TTx5ZVf-lA
 done
